8bd3b1ed1a51994d100cbfe0c29a09ed3653a2ac
xondamirzokirov <xondamirzokirov@gmail.com>
2022-04-02 10:15:18 +0500

    asd

--- app/src/main/java/com/example/bismillah/CategoryActivity.java
+++ app/src/main/java/com/example/bismillah/CategoryActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SeaAllActivity extends AppCompatActivity {
+public class CategoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sea_all);
+        setContentView(R.layout.activity_category);
     }
 }
